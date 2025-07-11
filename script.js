61f289675315064abdd40819ebbe307eec17016c
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-07-11 03:03:44 +0200

    start stuff

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Content rendern
+function init() {
+  renderall();
+}
 
 function renderBook1() {
-  let bookRef = document.getElementById("content-container1");
-  bookRef.innerHTML = "";
-  for (let index = 0; index < books.length; index++) {
+  //   let bookRef = document.getElementById("content-container1");
+  //   bookRef.innerHTML = "";
+  for (let index = 0; index < books.name.length; index++) {
     bookRef.innerHTML += getContent(index);
   }
 }
-
-// Container grob strukturieren
