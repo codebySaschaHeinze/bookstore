94b1440c9e9a814d670025b093fb5d6c58a3b664
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-07-13 19:08:04 +0200

    in the middle of the commentarie section. godd luck, future Sascha

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ function init() {
   // console.log(books);
 }
 
-// books in forSchleife
-
+// books in forSchleife (indexBooks beinhaltet alle Informationen von books (bg.js))
 function getBooks() {
   let bookRef = document.getElementById("content-container");
   bookRef.innerHTML = "";
@@ -13,7 +12,7 @@ function getBooks() {
   }
 }
 
-// comments in forSchleife
+// comments in forSchleife (weil die zwar in books sind, aber in extra Arrays)
 function getComments() {
   let commentRef = document.getElementById("content-container");
   commentRef.innerHTML = "";
@@ -29,26 +28,45 @@ function getComments() {
 }
 
 function changeLiked(indexBooks) {
-  books[indexBooks].liked = !books[indexBooks].liked;
-
+  // Entsprechender Funktionsaufruf, wenn true der Fall ist
   if (books[indexBooks].liked === true) {
+    // Wert ändert/aktuallisiert sich
+    books[indexBooks].liked = false;
+    minusALike(indexBooks);
     greenHeart(indexBooks);
+
+    // Ist true nicht der Fall, dann das hier
   } else {
+    books[indexBooks].liked = true;
+    plusALike(indexBooks);
     grayHeart(indexBooks);
   }
 }
 
-// Mit toggle könnte man sich eine Funktion sparen, aber das passt erstmal so
+// Mit toggle könnte man sich eine Funktion* sparen, aber das passt erstmal so
 function greenHeart(indexBooks) {
   let green = document.getElementById("green-heart-img" + indexBooks);
   let gray = document.getElementById("gray-heart-img" + indexBooks);
   green.classList.add("d_none");
   gray.classList.remove("d_none");
 }
-
+// *
 function grayHeart(indexBooks) {
   let gray = document.getElementById("gray-heart-img" + indexBooks);
   let green = document.getElementById("green-heart-img" + indexBooks);
   gray.classList.add("d_none");
   green.classList.remove("d_none");
+}
+
+// Bei Aufruf wird im entsprechenden Index +1 gezählt
+function plusALike(indexBooks) {
+  books[indexBooks].likes += 1;
+  let likesRef = document.getElementById("likes" + indexBooks);
+  likesRef.innerText = books[indexBooks].likes;
+}
+// Wie oben nur -1
+function minusALike(indexBooks) {
+  books[indexBooks].likes -= 1;
+  let likesRef = document.getElementById("likes" + indexBooks);
+  likesRef.innerText = books[indexBooks].likes;
 }
